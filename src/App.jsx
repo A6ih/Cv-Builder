dc42c78c2a3195e2cf885d5d2ede613938a89e71
A6ih <177154114+A6ih@users.noreply.github.com>
2025-11-25 00:55:18 +0200

    Add div to display preview of print option, Add @media rules for screen width and print

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,13 @@ import InputYear from "./InputYear"
 
 function App() {
   const [personalInfo, setPersonalInfo] = useState({name: "John Doe", profession:"Accountant", email: "johndoe@example.com", phone: 123456789})
-  const [summary, setSummary] = useState("");
+  const [summary, setSummary] = useState("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.");
   const [education, setEducation] = useState([{id: crypto.randomUUID(), degree: "BA Accounting", institution:"Mumbai University", yearStart:"2015", yearEnd:"2018"}])
-  const [work, setWork] = useState([{id: crypto.randomUUID(), role: "Accountant", company:"AC Firm", dateStart:"2019-01-02", dateEnd:"2022-01-01", description:""}])
-  const [skills, setSkills] = useState([{id: crypto.randomUUID(), name: ""}]);
+  const [work, setWork] = useState([{id: crypto.randomUUID(), role: "Accountant", company:"AC Firm", dateStart:"2019-01-02", dateEnd:"2022-01-01", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."}])
+  const [skills, setSkills] = useState([{id: crypto.randomUUID(), name: "Excel"}]);
   return (
-    <>
+   <>
+    <div id="edit-container">
      <Section header="General Information" className="general-info">
         <Input label="Name" text={personalInfo.name} handleInput={(e) => setPersonalInfo({...personalInfo, name:e.target.value})} inputType="text" placeholdText="Your Fullname"/>
         <Input label="Profession" text={personalInfo.profession} handleInput={(e) => setPersonalInfo({...personalInfo, profession:e.target.value})} inputType="text" placeholdText="Your Profession"/>
@@ -54,7 +55,7 @@ function App() {
                   <InputYear label="From" text={job.dateStart} handleInput={(e) => setWork(replaceInfo(work, job.id, e.target.value, "dateStart"))} inputType="date"/>
                   <InputYear label="To" text={job.dateEnd} handleInput={(e) => setWork(replaceInfo(work, job.id, e.target.value, "dateEnd"))} inputType="date"/>
                 </fieldset>
-                <textarea value={job.text} onChange={(e) => setWork(replaceInfo(work, job.id, e.target.value, "description"))} placeholder="Description" rows="4" cols="36"></textarea>
+                <textarea value={job.description} onChange={(e) => setWork(replaceInfo(work, job.id, e.target.value, "description"))} placeholder="Description" rows="4" cols="36"></textarea>
                 <button onClick={(e) => {
                   const parent = e.currentTarget.parentNode
                   parent.classList.add("card-delete")
@@ -71,18 +72,70 @@ function App() {
           skills.map(skill => {
             return(
               <div className="cards skill-cards" key={skill.id}>
-              <Input label="Skill" text={skill.name} handleInput={(e) => setSkills(replaceInfo(work, skill.id, e.target.value, "name"))} inputType="text" placeholdText="(eg: Excel)"/>
-              <button className="skill-delete" onClick={(e) => {
-                  const parent = e.currentTarget.parentNode
-                  parent.classList.add("card-delete")
-                  deleteField(setSkills, skills, skill.id)
-                }}>Delete</button>
+                <Input label="Skill" text={skill.name} handleInput={(e) => setSkills(replaceInfo(skills, skill.id, e.target.value, "name"))} inputType="text" placeholdText="(eg: Excel)"/>
+                <button className="skill-delete" onClick={(e) => {
+                    e.preventDefault()
+                    const parent = e.currentTarget.parentNode
+                    parent.classList.add("card-delete")
+                    deleteField(setSkills, skills, skill.id)
+                  }}>Delete</button>
               </div> 
             )
           })
         }
         <button onClick={() => setSkills(skills.concat(addSkill()))}>Add Skill</button>
      </Section>
+    </div>
+    <div id="preview-container">
+      <div>
+        <h2>{personalInfo.name}</h2>
+        <p className="preview-profession">{personalInfo.profession}</p>
+        <p className="preview-contact"><span>{personalInfo.phone}</span><span>{personalInfo.email}</span></p>
+      </div>
+      <div>
+        <h3>Summary</h3>
+        <p>{summary}</p>
+      </div>
+      <div>
+        <h3>Experience</h3>
+        {work.map(job => {
+          return (
+            <div key={job.id} className="preview-info-container">
+              <p className="preview-title">{job.role}</p>
+              <div className="preview-institution">
+                <p>{job.company}</p>
+                <p>{job.dateStart + " " + "To" + " " + job.dateEnd}</p>
+              </div>
+              <p>{job.description}</p>
+            </div>
+          )
+        })}
+      </div>
+      <div>
+        <h3>Education</h3>
+        {education.map(edu => {
+          return (
+            <div key={edu.id} className="preview-info-container">
+              <p className="preview-title">{edu.degree}</p>
+              <div className="preview-institution">
+                <p>{edu.institution}</p>
+                <p>{edu.yearStart + " " + "To" + " " + edu.yearEnd}</p>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+      <div>
+        <h3>Skills</h3>
+        <div id="preview-skills-container">
+          {skills.map(skill => {
+          return (
+            <p key={skill.id}>{skill.name}</p>
+          )
+        })}
+        </div>
+      </div>
+    </div>
     </>
   )
 }
